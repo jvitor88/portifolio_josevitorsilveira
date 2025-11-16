f6d553ff7e823288f31867f0c7e234ca07941c70
jvitor88 <80867252+jvitor88@users.noreply.github.com>
2025-11-16 02:37:17 -0300

    2025.2

--- src/components/aboutme/index.jsx
+++ src/components/aboutme/index.jsx
@@ -58,8 +58,8 @@ const AboutMe = () => {
                 <img src="./midia/images/Foto-perfil.jpg" alt="Foto de José Silveira" />
                 <StyledText>
                     <Subtitle>Sobre Mim</Subtitle>
-                    <p>Meu nome é José Vitor Silveira, tenho 27 anos, e sou profissional da área de varejo, atuando em áreas como BI, financeira e administrativa. Com foco em mensuração e agrupamento de informações de 11 unidades supermercadistas.</p>
-                    <p>Atuei na criação e apresentações de dashboards e DRE direta aos C-levels, além de desenvolver ETLs e indicadores-chave (KPIs) para suporte estratégico. Atualmente, estou concluindo uma pós-graduação em Analytics e Business Intelligence pela PUC Minas.</p>
+                    <p>Meu nome é José Vitor Silveira, sou Analista de Dados Sênior, formado em Análise de Sistemas e pós-graduado em Business Intelligence. Atuo na modelagem e integração de dados, no desenvolvimento de dashboards e KPIs e no suporte a projetos industriais de refino, gás e energia.
+                        Com foco em planejamento, fiscalização, análise de produtividade e metodologias AWP com modelos 3D, contribuindo para decisões mais rápidas e precisas em ambientes industriais.</p>
                 </StyledText>
             </StyledAbout>
         </section>
